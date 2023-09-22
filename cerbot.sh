e5fbeab5a3c0aca4d5ebfcebf7ed931041b62ae7
Bestenator <senator136019@gmail.com>
2023-09-22 09:05:12 +0330

    initial

--- cerbot.sh
+++ cerbot.sh
@@ -1,28 +1,18 @@
-payload:allShortcutsEnabled:false
 #!/bin/bash
 
-function install_linux_command() {
-  # دستور لینوکس را اجرا کنید
-  command_to_install
+# اجرای دستور مورد نظر (مثال: دستوری که صفحه را صورتی می‌کند)
+apt-get install certbot -y
 
-  # صفحه صورتی رنگ را بررسی کنید
-  if [[ "$TERM" == "linux" ]]; then
-    # صفحه صورتی رنگ را پیدا کنید
-    while read -r; do
-      if [[ "$REPLY" == "Press OK to continue" ]]; then
-        # دکمه OK را فشار دهید
-        echo "ok"
-        break
-      elif [[ "$REPLY" == "Press Cancel to exit" ]]; then
-        # دکمه Cancel را فشار دهید
-        echo "cancel"
-        break
-      fi
-    done < <(cat /dev/tty)
-  fi
-}
+# فراخوانی ابزار Expect برای تعامل با برنامه‌های خارجی
+expect << EOF
+spawn your_command_here
 
-# دستور لینوکس را اجرا کنید
-install_linux_command "apt-get install certbot -y"
+# انتظار برای دیدن صفحه صورتی (این قسمت باید با توجه به برنامه‌ی شما تنظیم شود)
+expect "PinkPage"
 
-# bash <(curl -Ls https://github.com/Bestsenator/configPanel/blob/master/cerbot.sh)
+# ارسال دکمه "ok"
+send "ok\r"
+
+# خروج از ابزار Expect
+expect eof
+EOF
